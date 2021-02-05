6c2a4bb0b0607f777e47ffdddb6369d9f4b174ec
Robin Grabs <robinmgrabs@gmail.com>
2021-02-05 22:37:24 +0100

    weather api working, weather details updates.

--- script/main.js
+++ script/main.js
@@ -4,10 +4,11 @@ let checkboxes = document.querySelectorAll('input[type=checkbox]');
 const apikey = "8b4c15d2d0638b590c32e373ba197bbb"
 
 document.querySelector('.search__term i').addEventListener('click', () => {
-    navigator.geolocation.getCurrentPosition((pos) => {
-        document.getElementById('searchterm').value = `${pos.coords.latitude}, ${pos.coords.longitude}`;
+    navigator.geolocation.getCurrentPosition(async (pos) => {
+        let returnObj = await apiCallLatLon(pos.coords.latitude, pos.coords.longitude);
+        document.getElementById('searchterm').value = returnObj.name;
+        document.getElementById('searchbutton').click();
     });
-
 })
 
 checkboxes.forEach(function(box) {
@@ -28,8 +29,8 @@ document.getElementById('searchbutton').addEventListener('click', async () => {
     if(input === '') {
         alert('Please enter a destination');
     } else {
-        let returnStr = await apiCall(input);
-        alert(returnStr);
+        let returnObj = await apiCallName(input);
+        updateWeather(returnObj);
     }
     if(optionsValues[0][1] === true) {
         document.querySelector('.results__weather').style.display = 'flex';
@@ -42,15 +43,33 @@ document.getElementById('searchbutton').addEventListener('click', async () => {
     if(optionsValues[1][1] === false && optionsValues[0][1] === false) {
         document.querySelector('.results__attractions').style.display = 'block';
         document.querySelector('.results__weather').style.display = 'flex';
-    }
-    
+    } 
 })
 
-async function apiCall(city) {
+async function apiCallName(city) {
     city.replace(' ', '%20');
     let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`);
     let json = await response.json();
-    return JSON.stringify(json);
+    return json;
 }
 
+async function apiCallLatLon(lat, lon) {
+    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}`);
+    let json = await response.json();
+    return json;
+}
 
+function updateWeather(obj) {
+    let weatherForm = document.querySelectorAll('.results__weather__details__item p');
+    document.querySelector('.results__weather__top h1').innerHTML = `Weather <br> - ${obj.name}`;
+    let weather = [
+        obj.weather[0].description,
+        `${Math.round((obj.main.temp - 273.15) * 10) / 10} C`,
+        `${obj.wind.speed} m/s`,
+        `${obj.clouds.all} %`,
+        `${obj.visibility / 10} m`,
+        `${obj.main.humidity / 10} %`
+    ];
+    document.getElementById('weatherimg').src = `http://openweathermap.org/img/wn/${obj.weather[0].icon.replace('n', 'd')}@2x.png`;
+    weatherForm.forEach((o,i) => { o.innerHTML = weather[i]; })
+}
