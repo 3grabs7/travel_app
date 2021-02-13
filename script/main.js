03946ce3e44bed6e91d4ac22d5743baef42f9bd0
Robin Grabs <robinmgrabs@gmail.com>
2021-02-13 11:28:06 +0100

    Image not found fixed, checkboxes grouped.

--- script/main.js
+++ script/main.js
@@ -1,9 +1,10 @@
 
 // Remove placeholder while search contains searchterm
-document.getElementById('searchterm').addEventListener('change', (e) => {
-    if(e.currentTarget.value != '') {
-        document.querySelector('#placeholder').innerHTML = "";
-    } else { document.querySelector('#placeholder').innerHTML = 'Enter your destination' }
+document.getElementById('searchterm').addEventListener('input', (e) => {
+    let placeholder = document.querySelector('#placeholder');
+    if(e.currentTarget.value === '') {
+       placeholder.innerHTML = 'Enter your destination';
+    } 
 })
 
 // Get ll and convert to name with location button
@@ -14,50 +15,75 @@ document.querySelector('.search__term i').addEventListener('click', () => {
         document.getElementById('searchterm').value = returnObj.name;
         document.getElementById('searchbutton').click();
     });
-    // Remove placeholder
-    document.querySelector('#placeholder').innerHTML = ""
+    document.querySelector('#placeholder').innerHTML = '';
 })
 
 // Event for checkboxes, both weather and attraction can't be true @same time
-let checkboxes = document.querySelectorAll('input[type=checkbox]');
+let checkboxes = Array.from(document.querySelectorAll('input[type=checkbox]')).slice(0,3);
 // Array to keep track of status of checkboxes
 let optionsValues = [['weather', false], ['attraction', false], ['sort', false]];
 checkboxes.forEach(function(box) {
     box.addEventListener('change', function(e) {
         let weather = document.querySelector('input[name=weather]');
         let attraction = document.querySelector('input[name=attraction]');
-        let sort = document.querySelector('input[name=sort]');
         if(e.currentTarget.name !== 'sort') {
             if(optionsValues[0][1] === true) { weather.checked = false; } 
             else if(optionsValues[1][1] === true) { attraction.checked = false; }
         } 
         optionsValues = [];
         checkboxes.forEach(b=> optionsValues.push([b.name ,b.checked]));
+        console.log(optionsValues);
     }) 
 })
 
 // Initate search, call methods for API calls
 document.getElementById('searchbutton').addEventListener('click', async () => {
     let input = document.getElementById('searchterm').value;
-    if(input === '') { alert('Please enter a destination'); } 
+    let placeholder = document.querySelector('#placeholder');
+    if(input === '') { 
+        // Placeholder blink animation when no user input is given
+        for(let i = 1; i < 6; i++) {
+            setTimeout(() => {
+                placeholder.style = 'opacity:0;transition opacity 0.5s';
+                setTimeout(() => {
+                    placeholder.style = 'opacity:1;transition opacity 0.5s';
+                }, 150 * i);
+            }, 150 * i);
+        }
+    } 
     else {
-        let returnObj = await apiCallName(input);
-        clearAttractions();
-        attraction(input);
-        updateWeather(returnObj);
+        // Make API calls and catch if response fails 
+        try {
+            let returnObj = await apiCallName(input);
+            clearAttractions();
+            attraction(input);
+            updateWeather(returnObj);
+            // Update placeholder
+            placeholder.innerHTML = '';
+        }
+        // Update placeholder to inform that search yielded no results
+        catch {
+            placeholder.innerHTML = 'Destination not found';
+            // Transition for placeholder, prompt user for search term
+            setTimeout(() => {
+                placeholder.innerHTML = 'Enter your destination';
+            }, 3000)
+        }
     }
+    let weather = document.querySelector('.results__weather');
+    let attractions = document.querySelector('.results__attractions')
     // Show results based on weather/attraction checkbox
     if(optionsValues[0][1] === true) {
-        document.querySelector('.results__weather').style.display = 'flex';
-        document.querySelector('.results__attractions').style.display = 'none';
+        weather.style.display = 'flex';
+        attractions.style.display = 'none';
     }
     if(optionsValues[1][1] === true) {
-        document.querySelector('.results__weather').style.display = 'none';
-        document.querySelector('.results__attractions').style.display = 'block';  
+        weather.style.display = 'none';
+        attractions.style.display = 'block';  
     }
     if(optionsValues[1][1] === false && optionsValues[0][1] === false) {
-        document.querySelector('.results__attractions').style.display = 'block';
-        document.querySelector('.results__weather').style.display = 'flex';
+        attractions.style.display = 'block';
+        weather.style.display = 'flex';
     } 
     if(optionsValues[2][1] === true) { sorted = true; }
     else { sorted = false; }
@@ -118,13 +144,13 @@ const token = `&client_id=${clientId}&client_secret=${clientSecret}&v=${today}`
 let attractionURL = 'https://api.foursquare.com/v2/venues/search?';
 // Array with APIs category ids and state of category checkboxes
 let categories = [
-    {name:'art_entertainment', id:'4d4b7104d754a06370d81259', checked:false, icon:''},
-    {name:'event', id:'4d4b7105d754a06373d81259', checked:false, icon:''},
-    {name:'food', id:'4d4b7105d754a06374d81259', checked:false, icon:''},
-    {name:'nightlife', id:'4d4b7105d754a06376d81259', checked:false, icon:''},
-    {name:'recreation', id:'4d4b7105d754a06377d81259', checked:false, icon:''},
-    {name:'shop_service', id:'4d4b7105d754a06378d81259', checked:false, icon:''},
-    {name:'travel_transport', id:'4d4b7105d754a06379d81259', checked:false, icon:''}
+    {name:'art_entertainment', id:'4d4b7104d754a06370d81259', checked:false},
+    {name:'event', id:'4d4b7105d754a06373d81259', checked:false},
+    {name:'food', id:'4d4b7105d754a06374d81259', checked:false},
+    {name:'nightlife', id:'4d4b7105d754a06376d81259', checked:false},
+    {name:'recreation', id:'4d4b7105d754a06377d81259', checked:false},
+    {name:'shop_service', id:'4d4b7105d754a06378d81259', checked:false},
+    {name:'travel_transport', id:'4d4b7105d754a06379d81259', checked:false}
         ];
 // Event listeners for checkboxes to update array with status 
 // Change background style when checked
@@ -146,11 +172,11 @@ for(let i = 0; i < catboxes[0].children.length; i++) {
     })
 }
 // API call to get venues
-async function attraction(city, load) {
+async function attraction(city) {
     // Update search for each category true, if all categories are unchecked, search in every category
     if(categories.filter(c=>c.checked === true).length === 0) {
         // Update query with search term
-        let query = `near=${city.replace(' ', '%20')}&limit=17&sortByPopularity=1`;
+        let query = `near=${city.replace(' ', '%20')}&limit=29&sortByPopularity=1`;
         // Send request
         let response = await fetch(`${attractionURL}${query}${token}`);
         // Await response and convert to json
@@ -188,7 +214,6 @@ async function attraction(city, load) {
             }
         }
     }
-    //loadAnimationAttraction();
 }
 // Clear attractions
 function clearAttractions() {
@@ -198,8 +223,7 @@ function clearAttractions() {
 function loadAttractions(obj) {
     let src = document.querySelector('.results__attractions__boxes');
     // If sorted checked -> sort venues
-    let sort = document.querySelector('input[name=sort]');
-    if(sort.checked === true) { 
+    if(optionsValues[2][1] === true) { 
         console.log('sort'); 
     }
     // Add attraction to search result
@@ -214,7 +238,13 @@ function loadAttractions(obj) {
         address.innerHTML = obj.address[i]?? 'No adress found';
         // Sizes 32, 44, 64, 88
         let icon = document.createElement('img');
-        icon.src = `${obj.icon[i].prefix}88${obj.icon[i].suffix}`;
+        if(typeof obj.icon[i] != 'string') {
+            icon.src = `${obj.icon[i].prefix}88${obj.icon[i].suffix}`
+        } else {
+            // Icon not found
+            icon.src = 'https://www.freeiconspng.com/uploads/exclamation-point-error-simple-black-background-pictures-18.png';
+            icon.style = 'filter:invert(1)';
+        }
         box.appendChild(head);
         box.appendChild(icon);
         box.appendChild(category);
